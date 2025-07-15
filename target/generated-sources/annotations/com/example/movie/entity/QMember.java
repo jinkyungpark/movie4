0b5873202abfc7c03bdd228218aa47b8726837cd
jinkyung <kyung5896@gmail.com>
2025-07-15 15:58:23 +0900

    소스수정

---
+++ target/generated-sources/annotations/com/example/movie/entity/QMember.java
@@ -0,0 +1,53 @@
+package com.example.movie.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QMember is a Querydsl query type for Member
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMember extends EntityPathBase<Member> {
+
+    private static final long serialVersionUID = -689307786L;
+
+    public static final QMember member = new QMember("member1");
+
+    public final QBaseEntity _super = new QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdDate = _super.createdDate;
+
+    public final StringPath email = createString("email");
+
+    public final EnumPath<MemberRole> memberRole = createEnum("memberRole", MemberRole.class);
+
+    public final NumberPath<Long> mid = createNumber("mid", Long.class);
+
+    public final StringPath nickname = createString("nickname");
+
+    public final StringPath password = createString("password");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedDate = _super.updatedDate;
+
+    public QMember(String variable) {
+        super(Member.class, forVariable(variable));
+    }
+
+    public QMember(Path<? extends Member> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QMember(PathMetadata metadata) {
+        super(Member.class, metadata);
+    }
+
+}
+
